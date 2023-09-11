8e0a30776ab68cf5cbc1fec11dec2d9f101ddf06
itsjay07 <113797398+itsjay07@users.noreply.github.com>
2023-09-11 21:25:11 +0530

    Add files via upload

---
+++ Lucky Numbers.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    bool isLucky(int n) {
+      for(int i=2; i<=n; i++)
+        {
+            if(n%i==0)
+            return false;
+            
+            n=n-n/i; // remmaining numbers
+        }
+        return true;
+  // code here
+    }
+};
