5efaef9e77cd54e57670cf81a2a2e91c71c89bbd
itsjay07 <113797398+itsjay07@users.noreply.github.com>
2023-10-13 21:16:58 +0530

    Add files via upload

---
+++ Floor in BST.cpp
@@ -0,0 +1,23 @@
+class Solution{
+
+public:
+    int floor(Node* root, int x){
+        
+        int ans = -1;
+
+        while (root) 
+        {
+            if (root->data == x)
+                return x;
+                
+            if (root->data > x)
+                root = root->left;
+                
+            else {
+                ans = root->data;
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
