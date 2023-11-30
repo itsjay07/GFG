e78c95dc8481572a67b9a270fb16f94c7f23600e
itsjay07 <113797398+itsjay07@users.noreply.github.com>
2023-11-30 13:01:28 +0530

    Add files via upload

---
+++ Shortest path from 1 to n.cpp
@@ -0,0 +1,20 @@
+class Solution{   
+public:
+    int minimumStep(int n){
+        
+        int steps = 0;
+
+        while (n > 1) 
+        {
+            if (n % 3 == 0) 
+            {
+                n /= 3;
+            } else {
+                n -= 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+};
